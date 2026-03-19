2d350b09ea6ab79900e4fc8617dd97315811e6e3
phesPhoenix <phoenixafschar@gmail.com>
2026-03-19 00:53:28 -0700

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
